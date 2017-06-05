e6ec79a40c107ed9f95f6c2e4da820a3e68bcaee
Claudia <cla.desantis93@gmail.com>
2017-06-05 11:23:49 +0200

    Inizio progetto

---
+++ galleriaArte/src/main/java/siw/progetto/galleriaArte/GalleriaArteApplication.java
@@ -0,0 +1,12 @@
+package siw.progetto.galleriaArte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalleriaArteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalleriaArteApplication.class, args);
+	}
+}
---
+++ galleriaArte/src/test/java/siw/progetto/galleriaArte/GalleriaArteApplicationTests.java
@@ -0,0 +1,16 @@
+package siw.progetto.galleriaArte;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GalleriaArteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
